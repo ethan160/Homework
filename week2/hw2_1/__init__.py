40e10162e6481faee5227f9ed32c3eec0c5abb87
ethan160 <ethanji1000@gmail.com>
2021-06-17 11:18:21 -0700

    add more items

---
+++ week2/hw2_1/__init__.py
@@ -0,0 +1,7 @@
+import math
+def roomsquare(width, length):
+    print(width*length)
+
+
+def cirOfcircle(r):
+    print(2 * math.pi * r)
---
+++ week2/hw2_1/main.py
@@ -0,0 +1,4 @@
+import hw2_1
+
+hw2_1.roomsquare(10, 10)
+hw2_1.cirOfcircle(10, 10)
